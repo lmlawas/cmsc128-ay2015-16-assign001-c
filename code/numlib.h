4560db2f7e3191133b68c385dc868fe79208fbc1
Leensey L <leenseylawas2007@gmail.com>
2016-02-08 15:44:38 +0800

    Added mainMenu() in test.c, Working numToWords() in numlib.h

--- code/numlib.h
+++ code/numlib.h
@@ -13,16 +13,93 @@ void printOnes(int n){
   else if(n==9) printf("nine ");
 }//end of printOnes()
 
+void printTens(int n){
+/******************************************************************************
+  Accepts a number 10<=n<=20 and number n who are multiples of
+  10 from 20 to 90; and prints out its word form.
+******************************************************************************/
+  if(n==10) printf("ten ");
+  else if(n==11) printf("eleven ");
+  else if(n==12) printf("twelve ");
+  else if(n==13) printf("thirteen ");
+  else if(n==14) printf("fourteen ");
+  else if(n==15) printf("fifteen ");
+  else if(n==16) printf("sixteen ");
+  else if(n==17) printf("seventeen ");
+  else if(n==18) printf("eighteen ");
+  else if(n==19) printf("nineteen ");
+  else if(n==2) printf("twenty");
+  else if(n==3) printf("thirty");
+  else if(n==4) printf("fourty");
+  else if(n==5) printf("fifty");
+  else if(n==6) printf("sixty");
+  else if(n==7) printf("seventy");
+  else if(n==8) printf("eighty");
+  else if(n==9) printf("ninety");
+}//end of printOnes()
+
 void numToWords(int num){
 /******************************************************************************
   Accepts a whole number from zero (0) to 1 million (1000000; withou commas)
   and prints on screen the number in word form.
 ******************************************************************************/
-  if(num>=1000000){
-    printOnes(num/1000000);
-    printf("million ");
-    num = num-(num/1000000);
+  if(!(num>=10000000)){
+    // millions
+    if(num>=1000000){
+      printOnes(num/1000000); // ex. 9m/1m = 9, printOnes(9) = nine
+      printf("million ");
+      num = num%1000000;  // remove the millions place
+    }
+    // hundred thousands
+    if(num>=100000){
+      printOnes(num/100000);  // ex. 200k/100k = 2, printOnes(2) = two
+      num = num%100000; // remove the hundred thousands place
+      printf("hundred ");
+      if(num<1000) printf("thousand "); // if no more thousands place number
+    }
+    // ten thousands
+    if(num>=10000 && num<=19999){
+      printTens(num/1000);  // ex. 13k/1k = 13, printTens(13) = thirteen
+      num = num%1000; // remove the ten thousands place
+      printf("thousand ");
+    }else if(num>=20000){
+      printTens(num/10000); // ex. 22k/10k = 2, printTens(2) = twenty
+      num = num%10000; //remove the ten thousands place
+      if(num>=1000){
+        printf("-");
+        printOnes(num/1000);  // ex. 2k/1k = 2, printOnes(2) = two
+        num = num%1000; //remove the ten thousands place
+      }else printf(" ");
+      printf("thousand ");
+    }
+    // thousands
+    if(num>=1000){
+      printOnes(num/1000);  // ex. 2k/1k = 2, printOnes(2) = two
+      num = num%1000; //remove the ten thousands place
+      printf("thousand ");
+    }
+    // hundreds
+    if(num>=100){
+      printOnes(num/100); // ex. 452/100 = 4, printOnes(4) = four
+      printf("hundred ");
+      num = num%100;  // remove the hundreds place
+    }
+    // tens
+    if(num>=10 && num<=19){
+      printTens(num);
+    }else if(num>=20){
+      printTens(num/10);  // ex. 64/10 = 6, printTens(6) = sixty
+      num = num%10; // remove the tens place
+      if(num>=1){
+        printf("-");
+        printOnes(num);
+        num = 0;
+      }
+    }
+    // ones
+    if(num>=1) printOnes(num);
   }
+  else printf("Error. Number too big.");
 
 }//end of numToWords()
 
@@ -31,7 +108,7 @@ void wordsToNum(){
   Accepts a number in word form (from zero to 1 million)
   and returns it in numerical form. Input must be in lowercase.
 ******************************************************************************/
-}
+}//end of wordsToNum()
 
 void wordsToCurrency(){
 /******************************************************************************
@@ -40,7 +117,7 @@ void wordsToCurrency(){
   JPY, PHP, USD. The function returns the number in words to its numerical
   form with a prefix of the currency.
 ******************************************************************************/
-}
+}//end of wordsToCurrency()
 
 void numberDelimited(){
 /******************************************************************************
@@ -49,4 +126,4 @@ void numberDelimited(){
   the number of jumps when the delimiter will appear (from right most
   going to left most digit)
 ******************************************************************************/
-}
+}//end of numberDelimited()
--- code/test.c
+++ code/test.c
@@ -1,7 +1,43 @@
 #include<stdio.h>
 #include "numlib.h"
 
+void mainMenu(int *choice){
+  printf("=======================\n");
+  printf("====== MAIN MENU ======\n");
+  printf("=======================\n");
+  printf("[1] number to words\n");
+  printf("[2] words to numbers\n");
+  printf("[3] words to currency\n");
+  printf("[4] number delimited\n");
+  printf("[0] exit\n");
+  printf("=======================\n");
+  printf("Choice:\t");
+  scanf("%d", choice);
+}//end of mainMenu()
+
 int main(){
-  printOnes(2);
+  int choice, num;
+  do{
+    mainMenu(&choice);
+    switch(choice){
+      case 1: // numbers to words
+        printf("\nEnter a 6-digit number:\n");
+        scanf("%d", &num);
+        numToWords(num);
+        printf("\n\n");
+        break;
+      case 2: // words to numbers
+        break;
+      case 3: //words to currency
+        break;
+      case 4: // number delimited
+        break;
+      case 0: // exit
+        printf("\nExit.\n\n");
+        break;
+      default: printf("Invalid choice. Try again.\n");
+    }
+  }while(choice!=0);
+
   return 0;
 }
